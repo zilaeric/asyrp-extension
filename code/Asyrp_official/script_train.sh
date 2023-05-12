530acfcd0925bce92cafc0d3c5fdbf1bdd6a014e
Jonathan Gerbscheid <jonathan-gerb@hotmail.com>
2023-05-12 11:18:11 +0200

    updates to job and sh files

--- code/Asyrp_official/script_train.sh
+++ code/Asyrp_official/script_train.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-sh_file_name="script_train.sh"
-gpu="0"
+SH_FILE_NAME="script_train.sh"
+GPU="0"
 
-config="custom.yml" # if you use other dataset, config/path_config.py should be matched
-guid="smiling" # guid should be in utils/text_dic.py
+CONFIG="celeba.yml" # if you use other dataset, config/path_config.py should be matched
+GUID="silly" # guid should be in utils/text_dic.py
+CUDA_VISIBLE_DEVICES=$GPU
 
-
-CUDA_VISIBLE_DEVICES=$gpu python main.py --run_train                        \
-                        --config $config                                    \
-                        --exp ./runs/$guid                                  \
-                        --edit_attr $guid                                   \
+python main.py --run_train                        \
+                        --config $CONFIG                                    \
+                        --exp ./runs/$GUID                                  \
+                        --edit_attr $GUID                                   \
                         --do_train 1                                        \
                         --do_test 1                                         \
-                        --n_train_img 100                                   \
-                        --n_test_img 32                                     \
+                        --n_train_img 10                                   \
+                        --n_test_img 10                                     \
                         --n_iter 5                                          \
                         --bs_train 1                                        \
                         --t_0 999                                           \
@@ -25,7 +25,7 @@ CUDA_VISIBLE_DEVICES=$gpu python main.py --run_train                        \
                         --user_defined_t_edit 500                           \
                         --user_defined_t_addnoise 200                       \
                         --train_delta_block                                 \
-                        --sh_file_name $sh_file_name                        \
+                        --sh_file_name $SH_FILE_NAME                        \
                         --save_x0                                           \
                         --use_x0_tensor                                     \
                         --hs_coeff_delta_h 1.0                              \
@@ -33,10 +33,9 @@ CUDA_VISIBLE_DEVICES=$gpu python main.py --run_train                        \
                         --clip_loss_w 1.0                                   \
                         --l1_loss_w 3.0                                     \
                         --retrain 1                                         \
-                        --custom_train_dataset_dir "test_images/celeba/train"       \
-                        --custom_test_dataset_dir "test_images/celeba/test"         \
-                        --sh_file_name "script_train.sh"
-
+                        --custom_train_dataset_dir "/home/lcur1654/DL2-2023-group-15/data/celeba_hq/raw_images/train/images"                \
+                        --custom_test_dataset_dir "/home/lcur1654/DL2-2023-group-15/data/celeba_hq/raw_images/test/images"                  \
+                        --model_path "/home/lcur1654/models/asyrp/pretrained/celeba_hq.pt"
                         # --add_noise_from_xt                               \ # if you compute lpips, use it.
                         # --lpips_addnoise_th 1.2                           \ # if you compute lpips, use it.
                         # --lpips_edit_th 0.33                              \ # if you compute lpips, use it.
