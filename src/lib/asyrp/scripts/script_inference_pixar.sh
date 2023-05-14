4d49fd52866840ce1df3559284bc3bee14c72e60
Jonathan Gerbscheid <jonathan-gerb@hotmail.com>
2023-05-14 17:08:04 +0200

    added updated scripts

--- src/lib/asyrp/scripts/script_inference_pixar.sh
+++ src/lib/asyrp/scripts/script_inference_pixar.sh
@@ -3,7 +3,7 @@
 sh_file_name="script_inference.sh"
 gpu="0"
 config="celeba.yml"
-guid="smiling"
+guid="pixar"
 test_step=50    # if large, it takes long time.
 dt_lambda=1.0   # hyperparameter for dt_lambda. This is the method that will appear in the next paper.
 CUDA_VISIBLE_DEVICES=$gpu
@@ -16,7 +16,8 @@ python main.py  --run_test                                                     \
                 --do_test 1                                                    \
                 --n_train_img 5                                                \
                 --n_test_img 5                                                 \
-                --bs_train 1                                                   \
+                --n_iter 5                                                     \
+                --bs_train 5                                                   \
                 --n_inv_step 50                                                \
                 --n_train_step 50                                              \
                 --n_test_step $test_step                                       \
@@ -25,9 +26,9 @@ python main.py  --run_test                                                     \
                 --sh_file_name $sh_file_name                                   \
                 --save_x0                                                      \
                 --use_x0_tensor                                                \
-                --hs_coeff_delta_h 1.0                                         \
+                --hs_coeff_delta_h 0.5                                         \
                 --dt_lambda $dt_lambda                                         \
-                --manual_checkpoint_name "smiling_LC_CelebA_HQ_t999_ninv40_ngen40_0.pth"    \
+                --manual_checkpoint_name "pixar_LC_CelebA_HQ_t999_ninv40_ngen40_0.pth"    \
                 --add_noise_from_xt                                            \
                 --lpips_addnoise_th 1.2                                        \
                 --lpips_edit_th 0.33                                           \
